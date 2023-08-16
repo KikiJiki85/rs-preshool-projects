d177ac0074400367ab678a3a057fe0fc48222b5c
Aleksey Vaskin <alzor85@gmail.com>
2023-08-16 18:13:39 +0300

    feat: add About slider js logic

--- library/assets/js/index.js
+++ library/assets/js/index.js
@@ -34,15 +34,22 @@ console.log(``);
 
 
 let position = 0;
-const slidesToShow = 3;
+let slidesToShow = 3;
+let activeButton = 0;
 const slidesToScroll = 1;
 
 const slider = document.querySelector('.slider');
+const mediaQuery = window.matchMedia('(max-width: 1024px)');
+if(mediaQuery.matches) {
+   slidesToShow = 1;
+}
 
 const container = slider.querySelector('.slider-wrapper');
 const track = slider.querySelector('.slider-list');
 const items = slider.querySelectorAll('.slider-item');
 const sliderControls = slider.querySelectorAll('.slider-control');
+const tabletPrev = slider.querySelector('.slider-control-tablet1');
+const tabletNext = slider.querySelector('.slider-control-tablet2');
 
 const itemsCount = items.length;
 const itemWidth = container.clientWidth / slidesToShow;
@@ -52,28 +59,40 @@ items.forEach((item) => {
    item.style.minWidth = `${itemWidth}px`;
 })
 
-sliderControls[1].addEventListener('click', () => {
-   const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
-
-   position -= itemsLeft >= slidesToScroll ?  movePosition : itemsLeft * itemWidth;
-
-   setPosition();
-});
-
-sliderControls[0].addEventListener('click', () => {
-   const itemsLeft = Math.abs(position) / itemWidth;
-
-   position += itemsLeft >= slidesToScroll ?  movePosition : itemsLeft * itemWidth;
-
-   setPosition();
+sliderControls.forEach((el,i) => {
+   el.addEventListener('click', (evt) => {
+      position = -itemWidth * i;
+      setPosition();
+      setActiveButton(evt,el);
+   });
 });
 
 const setPosition = () => {
    track.style.transform = `translateX(${position}px)`;
 };
 
-const checkBtns = () => {
+const setActiveButton = (evt,el) => {
+   if (el.classList.contains('active-slide')) {
+      el.classList.remove('active-slide')
+   } else {
+      evt.target.classList.add('active-slide');
+   }
 };
 
+
+
+tabletPrev.addEventListener('click', () => {
+   const itemsLeft = Math.abs(position) / itemWidth;
+
+   position += itemsLeft >= slidesToScroll ?  movePosition : itemsLeft * itemWidth;
+   setPosition();
+});
+
+tabletNext.addEventListener('click', () => {
+   const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+
+   position -= itemsLeft >= slidesToScroll ?  movePosition : itemsLeft * itemWidth;
+   setPosition();
+});
 
 
