4f0bbace27b1c3db3c04617331bd69b94eed7c33
Aleksey Vaskin <alzor85@gmail.com>
2023-09-20 17:57:35 +0300

    fix: image container size, switching logic

--- js30#1.2-audio-player/assets/js/index.js
+++ js30#1.2-audio-player/assets/js/index.js
@@ -7,12 +7,13 @@ const progressContainer = player.querySelector('.music-app__progress-container')
 const progress = player.querySelector('.music-app__progress');
 const currentSong = player.querySelector('.music-app__current-song');
 const coverImg = player.querySelector('.music-app__cover-img');
-const status = player.querySelector('.music-app__status');
+const imgStatus = player.querySelector('.music-app__status');
 
 const songs = [`Beyonce - Don't Hurt Yourself`,`Dua Lipa - Don't Start Now`];
 
 let songIndex = 0;
 let isPlaying = false;
+let isChanging = false;
 
 function loadSong(song) {
     currentSong.textContent = song;
@@ -20,18 +21,43 @@ function loadSong(song) {
     track.src = `./assets/audio/${song}.mp3`;
 }
 
+function updateSong() {
+    if(songIndex < 0) songIndex = songs.length - 1;
+    else if (songIndex > songs.length - 1) songIndex = 0;
+    loadSong(songs[songIndex]);
+}
+
 function playSong() {
-    if (!isPlaying) {
+    if (!isPlaying || isChanging) {
         track.play();
         isPlaying = true;
+        imgStatus.src = `./assets/img/pause.png`;
+        isChanging = false;
     } else {
         track.pause();
         isPlaying = false;
+        imgStatus.src = `./assets/img/play.png`;
     }
 }
 
+function playPrev() {
+    songIndex--;
+    updateSong();
+    isChanging = true;
+    playSong();
+
+}
+
+function playNext() {
+    songIndex++;
+    updateSong();
+    isChanging = true;
+    playSong();
+}
+
 playBtn.addEventListener('click', playSong);
+prevBtn.addEventListener('click', playPrev);
+nextBtn.addEventListener('click', playNext);
 
 loadSong(songs[songIndex]);
-
 
