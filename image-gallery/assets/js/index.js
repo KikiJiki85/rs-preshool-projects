36cfcda8b64118e47086c9c297dcb15dffc05867
Aleksey Vaskin <alzor85@gmail.com>
2023-09-29 12:38:31 +0300

    refactor: change render function

--- image-gallery/assets/js/index.js
+++ image-gallery/assets/js/index.js
@@ -1,10 +1,12 @@
+const URL = `https://api.unsplash.com/photos/random?client_id=McvrzyhY5VI1bgjT8uWsKZpIywG3VrfhN6mT_Z9YCP8&count=28&query=`;
+
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('.header__form');
     const search = form.querySelector('.header__search');
     const imageGallery = document.querySelector('.image-gallery');
 
     async function getImages(query = '') {
-        const res = await fetch(`https://api.unsplash.com/photos/random?client_id=McvrzyhY5VI1bgjT8uWsKZpIywG3VrfhN6mT_Z9YCP8&count=28&query=${query}`);
+        const res = await fetch(`${URL}${query}`);
         return await res.json();
       }
 
@@ -12,18 +14,30 @@ document.addEventListener('DOMContentLoaded', () => {
         try {
           data.then(el => {
             el.forEach(({ urls, links }) => {
-              const img = document.createElement('img');
-              const a = document.createElement('a');
-    
+              const fragment = document.createDocumentFragment();
+              
+              const a = fragment.appendChild(document.createElement('a'));
+              const img = a.appendChild(document.createElement('img'));
               a.classList.add('image-gallery__link');
               img.classList.add('image-gallery__img');
-    
               a.href = links.download;
               a.target = '_blank';
-              img.src = urls.small;
+              img.src = urls.regular;
+              console.log(urls);
+              imageGallery.appendChild(fragment);
+
+              // const img = document.createElement('img');
+              // const a = document.createElement('a');
     
-              a.append(img);
-              imageGallery.append(a);
+              // a.classList.add('image-gallery__link');
+              // img.classList.add('image-gallery__img');
+    
+              // a.href = links.download;
+              // a.target = '_blank';
+              // img.src = urls.small;
+    
+              // a.append(img);
+              // imageGallery.append(a);
             })
           })
         } catch (err) {
