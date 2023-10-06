5f22f78ce6c8228e660df9db27964dac429b66fa
Aleksey Vaskin <alzor85@gmail.com>
2023-10-06 18:37:40 +0300

    feat: add more app css

--- random-game/assets/js/index.js
+++ random-game/assets/js/index.js
@@ -1,6 +1,27 @@
-const menu = document.querySelector('.ttt-app__menu');
-const menuItems = menu.querySelector('.ttt-app__menu-items');
+const menu = document.querySelector('[data-id="menu"]');
+const menuItems = menu.querySelector('[data-id="menu__items"]');
+const resetBtn = menu.querySelector('[data-id="menu__reset"]');
+const newRoundBtn = menu.querySelector('[data-id="menu__new-round"]');
+const fields = document.querySelectorAll('[data-id="field"]');
 
-menu.addEventListener('click', () => {
-    menuItems.classList.toggle('modal-show');
-});
+
+function registerEventListeners() {
+    menu.addEventListener('click', () => {
+        menuItems.classList.toggle('modal-show');
+    });
+    
+    resetBtn.addEventListener('click', () => {
+        console.log('Reset the game');
+    });
+    
+    newRoundBtn.addEventListener('click', () => {
+        console.log('New round');
+    });
+    
+    fields.forEach((el) => 
+        el.addEventListener('click', (evt) => {
+            console.log(`click on ${evt.target.id} element`);
+    }));
+}
+
+document.addEventListener('DOMContentLoaded', registerEventListeners);
