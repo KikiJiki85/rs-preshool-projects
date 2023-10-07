81a5d9684232babb334b18c3479ad637dfa71598
Aleksey Vaskin <alzor85@gmail.com>
2023-10-07 16:12:30 +0300

    feat: add playing logic functions

--- random-game/assets/js/index.js
+++ random-game/assets/js/index.js
@@ -5,8 +5,39 @@ const newRoundBtn = menu.querySelector('[data-id="menu__new-round"]');
 const fields = document.querySelectorAll('[data-id="field"]');
 
 const state = {
-    currentPlayer: 1,
+    moves: []
 };
+
+const winningPatterns = [
+    [1,2,3],
+    [1,5,9],
+    [1,4,7],
+    [2,5,8],
+    [3,5,7],
+    [3,6,9],
+    [4,5,6],
+    [7,8,9]
+];
+
+function getGameStatus(moves) {
+    const player1Moves = moves.filter((move) => move.player === 1).map(move => Number(move.fieldId));
+    const player2Moves = moves.filter((move) => move.player === 2).map(move => Number(move.fieldId));
+
+    let winner = null;
+
+    winningPatterns.forEach((pattern) => {
+        const player1Wins = pattern.every(value => player1Moves.includes(value));
+        const player2Wins = pattern.every(value => player2Moves.includes(value));
+
+        if(player1Wins) winner = 1;
+        if(player2Wins) winner = 2;
+    });
+
+    return {
+        status: moves.length === 9 || winner !== null ? 'finish' : 'in-progress',
+        winner,
+    }
+}
 
 
 function registerEventListeners() {
@@ -24,14 +55,19 @@ function registerEventListeners() {
     
     fields.forEach((field) => 
         field.addEventListener('click', (evt) => {
-            console.log(`click on ${evt.target.id} element`);
+           
+            const hasMove = (fieldId) => {
+                const madeMove = state.moves.find((move) => move.fieldId === fieldId);
+                return madeMove !== undefined;
+            };
 
-            if (field.hasChildNodes()) {
-                console.log(field.childNodes);
-                return;
-            }
+            if (hasMove(Number(field.id))) return;
 
-            const currentPlayer = state.currentPlayer;
+            const lastMove = state.moves.at(-1); // equals - state.moves[moves.length - 1]
+            const getNextPlayer = (player) => player === 1 ? 2 : 1;
+            const currentPlayer = state.moves.length === 0 ? 1 : getNextPlayer(lastMove.player);
+
+
             const icon = document.createElement('span');
 
             if (currentPlayer === 1) {
@@ -40,13 +76,23 @@ function registerEventListeners() {
                 icon.classList.add('ttt-app__o-icon');
             }
 
-            state.currentPlayer = state.currentPlayer === 1 ? 2 : 1;
-            
+            state.moves.push({
+                player: currentPlayer,
+                fieldId: Number(field.id)
+            });
             
             field.replaceChildren(icon);
 
-            // <span class="ttt-app__x-icon"></span>
-            // <span class="ttt-app__o-icon"></span>
+            const game = getGameStatus(state.moves);
+
+            if (game.status === 'finish') {
+                if (game.winner) {
+                    alert(`Player ${game.winner} wins!`)
+                } else {
+                    alert('Its a tie');
+                }
+            }
+
     }));
 }
 
